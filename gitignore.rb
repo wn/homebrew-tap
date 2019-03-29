b9125ed66e751d063dc6a0489a434be27196a2bb
wn <weineng.a@gmail.com>
2019-03-30 02:30:20 +0800

    Set version to 1.1

--- gitignore.rb
+++ gitignore.rb
@@ -3,15 +3,14 @@ class Gitignore < Formula
   homepage "https://github.com/wn/gitignore"
   url "https://github.com/wn/gitignore/blob/master/gitignore-1.0.tar.gz?raw=true"
   sha256 "3a746ab5e4e3045e64835b0593066f8c42adfeb35bf1aea4d1ed5d65432c0815"
-  version "1.0"
+  version "1.1"
 
   def install
-    # install hworld script, create symlink to script in /usr/local/bin
+    # install gitignore script, create symlink to script in /usr/local/bin
     bin.install "gitignore"
   end
 
   test do
-    # test script output
-    assert_equal %x('#{bin}/hworld'), "Hello world!\n"
+
   end
 end
