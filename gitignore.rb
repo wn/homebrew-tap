4404ea914fd16f3d29aeadeab9189c3b8b7de9c2
wn <weineng.a@gmail.com>
2019-03-31 17:10:47 +0800

    Fix audit command

--- gitignore.rb
+++ gitignore.rb
@@ -1,9 +1,9 @@
 class Gitignore < Formula
-  desc "Script to generate .gitignore files for a specified language."
+  desc "Script to generate .gitignore files for a specified language"
   homepage "https://github.com/wn/gitignore"
   url "https://github.com/wn/gitignore/blob/master/gitignore-1.2.tar.gz?raw=true"
-  sha256 "c2665ec3fe39b7efe419e3f1802eace6d39ed0774c9fc9f31f415a78ae61b35b"
   version "1.2"
+  sha256 "c2665ec3fe39b7efe419e3f1802eace6d39ed0774c9fc9f31f415a78ae61b35b"
 
   def install
     # install gitignore script, create symlink to script in /usr/local/bin
